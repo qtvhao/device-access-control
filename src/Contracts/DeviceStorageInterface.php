b4a91b9151a64a63f4800631236db8ab3edaf32f
Your Name <you@example.com>
2024-11-11 12:21:51 +0100

    Remove DeviceStorageInterface as it is no longer needed

--- src/Contracts/DeviceStorageInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Qtvhao\DeviceAccessControl\Contracts;
-
-interface DeviceStorageInterface
-{
-    public function getDevice($userId, $deviceType);
-    public function storeDevice($userId, $deviceId, $deviceType);
-    public function removeDevice($userId, $deviceType);
-}
