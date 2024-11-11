7f2b084a4967e3d323a910af7363f80b3af6bbca
Your Name <you@example.com>
2024-11-11 20:02:07 +0100

    Refactor DeviceAccessControlServiceProvider to streamline migration publishing

--- Providers/DeviceAccessControlServiceProvider.php
+++ Providers/DeviceAccessControlServiceProvider.php
@@ -5,8 +5,6 @@ namespace Qtvhao\DeviceAccessControl\Providers;
 use Illuminate\Support\ServiceProvider;
 use Qtvhao\DeviceAccessControl\Repository\DeviceAccessRepository;
 use Qtvhao\DeviceAccessControl\Core\Interfaces\DeviceAccessRepositoryInterface;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Collection;
 
 
 class DeviceAccessControlServiceProvider extends ServiceProvider
@@ -19,23 +17,8 @@ class DeviceAccessControlServiceProvider extends ServiceProvider
     }
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../database/migrations/2024_11_11_000000_create_device_table.php' => $this->getMigrationFileName('create_device_table.php'),
-        ], 'device-migrations');
-    }
-
-    /**
-     * Returns existing migration file if found, else uses the current timestamp.
-     */
-    protected function getMigrationFileName(string $migrationFileName): string
-    {
-        $timestamp = date('Y_m_d_His');
-
-        $filesystem = $this->app->make(Filesystem::class);
-
-        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
-            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
-            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
-            ->first();
+        $this->publishesMigrations([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ]);
     }
 }
