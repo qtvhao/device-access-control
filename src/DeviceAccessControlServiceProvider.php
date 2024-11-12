ee9480e51b18d1111f29478363643c886905b581
Your Name <you@example.com>
2024-11-12 04:59:31 +0100

    Bind CheckDeviceLimitUseCase in DeviceAccessControlServiceProvider for device limit management

--- src/DeviceAccessControlServiceProvider.php
+++ src/DeviceAccessControlServiceProvider.php
@@ -8,6 +8,7 @@ use Qtvhao\DeviceAccessControl\Core\Interfaces\DeviceAccessRepositoryInterface;
 use Qtvhao\DeviceAccessControl\Model\Device as DeviceModel;
 use Illuminate\Routing\Router;
 use Qtvhao\DeviceAccessControl\Middleware\DeviceAccessMiddleware;
+use Qtvhao\DeviceAccessControl\Core\UseCases\CheckDeviceLimitUseCase;
 
 class DeviceAccessControlServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,12 @@ class DeviceAccessControlServiceProvider extends ServiceProvider
     {
         $this->app->bind(DeviceAccessRepositoryInterface::class, function ($app) {
             return new DeviceAccessRepository(new DeviceModel());
+        });
+        $this->app->bind(CheckDeviceLimitUseCase::class, function ($app) {
+            return new CheckDeviceLimitUseCase(
+                $app->make(DeviceAccessRepositoryInterface::class),
+                config('device_access_control.device_limit', 1) // lấy device_limit từ config, default là 1 nếu không có
+            );
         });
     }
 
