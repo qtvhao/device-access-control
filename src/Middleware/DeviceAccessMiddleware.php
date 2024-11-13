799c193c9d203183a02a47d1aebff13246e7851f
Your Name <you@example.com>
2024-11-13 06:47:00 +0100

    Refactor DeviceAccessMiddleware to utilize JWTAuth for improved token handling and device information retrieval

--- src/Middleware/DeviceAccessMiddleware.php
+++ src/Middleware/DeviceAccessMiddleware.php
@@ -6,16 +6,22 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Qtvhao\DeviceAccessControl\Core\UseCases\DeviceAccessOrchestrator;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWT;
+use Qtvhao\DeviceAccessControl\Core\Data\DeviceData;
 
 class DeviceAccessMiddleware
 {
     protected $orchestrator;
+    /**
+     * @var JWTAuth|JWT
+     */
+    protected $jwtAuth;
 
-    public function __construct(DeviceAccessOrchestrator $orchestrator)
+    public function __construct(DeviceAccessOrchestrator $orchestrator, JWTAuth $jwtAuth)
     {
         $this->orchestrator = $orchestrator;
+        $this->jwtAuth = $jwtAuth;
     }
-
     /**
      * Handle an incoming request.
      *
@@ -29,13 +35,18 @@ class DeviceAccessMiddleware
             return new Response('The token could not be parsed from the request', Response::HTTP_BAD_REQUEST);
         }
         // Try to get the authenticated user from the token
-        if (!$user = JWTAuth::parseToken()->authenticate()) {
+        if (!$user = $this->jwtAuth->parseToken()->authenticate()) {
             return new Response('Không có quyền truy cập', Response::HTTP_UNAUTHORIZED);
         }
 
+        $jwtPayload = $this->jwtAuth->getPayload();
+        $deviceInfo = $jwtPayload?->dev ?? null;
+        if (!$deviceInfo || !is_array($deviceInfo)) {
+            return new Response('Thiếu thông tin thiết bị', Response::HTTP_BAD_REQUEST);
+        }
         // Get the device ID from the JWT payload or request headers
-        $deviceId = $request->header('Device-Id') ?? JWTAuth::getPayload()->get('device_id');
-        $deviceType = $request->header('Device-Type') ?? JWTAuth::getPayload()->get('device_type');
+        $deviceId = $deviceInfo['id'];
+        $deviceType = $deviceInfo['type'];
 
         if (!$deviceId || !$deviceType) {
             // Return an error if headers are missing
@@ -44,7 +55,12 @@ class DeviceAccessMiddleware
 
         // Execute device check
         $userId = $user->id;
-        $isAllowed = $this->orchestrator->execute($userId, $deviceId, $deviceType);
+        $isAllowed = $this->orchestrator->execute(new DeviceData(
+            deviceId: $deviceId,
+            deviceType: $deviceType,
+            deviceName: $request->header('User-Agent'),
+            userId: $userId
+        ));
 
         if (!$isAllowed) {
             // If access is denied due to device limit, return an error response
