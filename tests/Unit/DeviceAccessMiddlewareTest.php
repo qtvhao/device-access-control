9f57a1f0d5a8b1b0533a5541587de33e518e32fd
Your Name <you@example.com>
2024-11-13 06:36:15 +0100

    Refactor DeviceAccessMiddlewareTest to include JWT authentication and improve device access validation

--- tests/Unit/DeviceAccessMiddlewareTest.php
+++ tests/Unit/DeviceAccessMiddlewareTest.php
@@ -8,20 +8,32 @@ use Qtvhao\DeviceAccessControl\Middleware\DeviceAccessMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Mockery;
+use Qtvhao\DeviceAccessControl\Core\Enums\DeviceEnums;
 
 class DeviceAccessMiddlewareTest extends TestCase
 {
+    private $userId = 1;
     public function test_device_access_allowed()
     {
         $orchestratorMock = Mockery::mock(DeviceAccessOrchestrator::class);
         $orchestratorMock->shouldReceive('execute')
-                         ->andReturn(true);
+            ->andReturn(true);
 
-        $middleware = new DeviceAccessMiddleware($orchestratorMock);
-        $request = Request::create('/learning', 'GET', [], [], [], ['HTTP_Device-ID' => 'abc123', 'HTTP_Device-Type' => 'Web'])
-            ->setUserResolver(function () {
-                return (object) ['id' => 1];
-            });
+        $jwtAuthMock = Mockery::mock('alias:Tymon\JWTAuth\Facades\JWTAuth');
+        $jwtAuthMock->shouldReceive('parseToken')->andReturnSelf();
+        $jwtAuthMock->shouldReceive('authenticate')->andReturn((object) ['id' => $this->userId]);
+        $jwtAuthMock->shouldReceive('getPayload')->andReturn((object) [
+            'dev' => [
+                'id' => 'abc123',
+                'type' => DeviceEnums::DEVICE_TYPE_WEB
+            ]
+        ]);
+         
+
+        $middleware = new DeviceAccessMiddleware($orchestratorMock, $jwtAuthMock);
+        $request = Request::create('/learning', 'GET', [], [], [], [
+            'HTTP_Authorization' => 'Bearer some-valid-token'
+        ]);
         
         $response = $middleware->handle($request, function () {
             return new Response('Access Granted');
@@ -37,11 +49,19 @@ class DeviceAccessMiddlewareTest extends TestCase
         $orchestratorMock->shouldReceive('execute')
                          ->andReturn(false);
 
-        $middleware = new DeviceAccessMiddleware($orchestratorMock);
-        $request = Request::create('/learning', 'GET', [], [], [], ['HTTP_Device-ID' => 'abc123', 'HTTP_Device-Type' => 'Web'])
-            ->setUserResolver(function () {
-                return (object) ['id' => 1];
-            });
+        $jwtAuthMock = Mockery::mock('alias:Tymon\JWTAuth\Facades\JWTAuth');
+        $jwtAuthMock->shouldReceive('parseToken')->andReturnSelf();
+        $jwtAuthMock->shouldReceive('authenticate')->andReturn((object) ['id' => $this->userId]);
+        $jwtAuthMock->shouldReceive('getPayload')->andReturn((object) [
+            'dev' => [
+                'id' => 'abc123',
+                'type' => DeviceEnums::DEVICE_TYPE_WEB
+            ]
+        ]);
+        $middleware = new DeviceAccessMiddleware($orchestratorMock, $jwtAuthMock);
+        $request = Request::create('/learning', 'GET', [], [], [], [
+            'HTTP_Authorization' => 'Bearer some-valid-token'
+        ]);
         
         $response = $middleware->handle($request, function () {
             return new Response('Access Granted');
@@ -49,5 +69,10 @@ class DeviceAccessMiddlewareTest extends TestCase
 
         $this->assertEquals(403, $response->getStatusCode());
         $this->assertEquals('Vượt quá giới hạn thiết bị truy cập', $response->getContent());
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
     }
 }
